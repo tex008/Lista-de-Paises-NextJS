baa44a96b7a1800440dea3b7773870d3d7172c9c
tex008 <texera009@gmail.com>
2023-08-16 10:55:19 -0300

    feat: create api requisition in the page component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito_sans.className}>
-        <main className='bg-gray-100 min-h-screen'>
-          <nav className='w-full bg-white h-16'>
-            <section className='container flex items-center justify-center gap-3'>
+        <main className='bg-gray-100 min-h-screen flex flex-col items-center'>
+          <nav className='w-full bg-white h-16 flex items-center justify-center'>
+            <section className='container flex items-center gap-3'>
               <Image width={48} height={48} src="/logo.svg" alt="logo da aplicação globo terrestre"/>
               <h1 className='font-bold text-2xl'>Lista de países</h1>
             </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Image from 'next/image'
+type Country = {
+  name: {
+    common: string;
+  }
+}
 
-export default function Home() {
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json()
+}
+
+export default async function Home() {
+  const countries = await getCountries()
+  console.log("teste", countries)
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>no nextJs</h2>
-    </div>
+    <section className="container flex w-full">
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ) )}
+    </section>
   )
 }
