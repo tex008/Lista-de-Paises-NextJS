3b8d89ffb2a8e137dc392a4d1f3360ed36d0c361
tex008 <texera009@gmail.com>
2023-08-16 10:04:31 -0300

    feat: create basic layout, adding the nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
+import Image from 'next/image'
 import './globals.css'
 
 const nunito_sans = Nunito_Sans({ subsets: ['latin'] })
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunito_sans.className}>{children}</body>
+      <body className={nunito_sans.className}>
+        <main className='bg-gray-100 min-h-screen'>
+          <nav className='w-full bg-white h-16'>
+            <section className='container flex items-center justify-center gap-3'>
+              <Image width={48} height={48} src="/logo.svg" alt="logo da aplicação globo terrestre"/>
+              <h1 className='font-bold text-2xl'>Lista de países</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
