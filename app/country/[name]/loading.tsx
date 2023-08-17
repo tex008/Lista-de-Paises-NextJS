a624428997915ceb78779d62b7d530996183cba7
tex008 <texera009@gmail.com>
2023-08-17 10:24:41 -0300

    feat: create a loading page for the specific country page

---
+++ app/country/[name]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <section className="flex items-center gap-3 mt-12">
+      <h1 className="
+      text-center text-5xl font-bold text-gray-800"> Loading...</h1>
+     
+    </section>
+  )
+}
